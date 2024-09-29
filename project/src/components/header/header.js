8a367319dabd74d151ab49b4e72760e8f0d70d6b
Abhijit <abhijitdatta54321@gmail.com>
2024-09-30 00:18:45 +0530

    commit 53

--- project/src/components/header/header.js
+++ project/src/components/header/header.js
@@ -1,33 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "./header.css";
 import logo from "../Assets2/logo.jpg";
-import { Drawer, IconButton, List, ListItem, ListItemText, Divider } from "@mui/material";
+import {
+  Drawer,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu"; // Hamburger icon
 import CloseIcon from "@mui/icons-material/Close"; // Close (Cross) icon
 import { Link, useLocation } from "react-router-dom";
 
 const Header = ({ onProductsClick }) => {
-  const [drawerOpen, setDrawerOpen] = useState(false);  // Drawer open state
-  const location = useLocation();  // Detect route changes
+  const [drawerOpen, setDrawerOpen] = useState(false); // Drawer open state
+  const location = useLocation(); // Detect route changes
 
   // Effect to close the drawer when the route changes
   useEffect(() => {
-    setDrawerOpen(false);  // Close drawer on route change
-  }, [location]);  // Runs when the location (route) changes
+    setDrawerOpen(false); // Close drawer on route change
+  }, [location]); // Runs when the location (route) changes
 
   // Toggle drawer open/close
   const toggleDrawer = (open) => (event) => {
-    if (event.type === "keydown" && (event.key === "Tab" || event.key === "Shift")) {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
       return;
     }
-    setDrawerOpen(open);  // Open or close the drawer based on the value of `open`
+    setDrawerOpen(open); // Open or close the drawer based on the value of `open`
   };
 
   // Drawer list
   const drawerList = () => (
     <div
       role="presentation"
-      onClick={toggleDrawer(false)}   // Close drawer on outside click
+      onClick={toggleDrawer(false)} // Close drawer on outside click
       onKeyDown={toggleDrawer(false)} // Close drawer with keyboard navigation
       className="drawer-content"
     >
@@ -37,21 +47,41 @@ const Header = ({ onProductsClick }) => {
       </IconButton>
 
       <List>
-        <ListItem button component={Link} to="/" onClick={() => setDrawerOpen(false)}>
+        <ListItem
+          button
+          component={Link}
+          to="/"
+          onClick={() => setDrawerOpen(false)}
+        >
           <ListItemText primary="Home" />
         </ListItem>
-        <ListItem button component={Link} to="/products" onClick={() => {
-          onProductsClick();  // Trigger smooth scroll to products
-          setDrawerOpen(false);  // Close the drawer
-        }}>
+        {/* <ListItem
+          button
+          component={Link}
+          to="/products"
+          onClick={() => {
+            onProductsClick();
+            setDrawerOpen(false);
+          }}
+        >
           <ListItemText primary="Products" />
         </ListItem>
-        <ListItem button component={Link} to="/about" onClick={() => setDrawerOpen(false)}>
+        <ListItem
+          button
+          component={Link}
+          to="/about"
+          onClick={() => setDrawerOpen(false)}
+        >
           <ListItemText primary="About" />
         </ListItem>
-        <ListItem button component={Link} to="/contact" onClick={() => setDrawerOpen(false)}>
+        <ListItem
+          button
+          component={Link}
+          to="/contact"
+          onClick={() => setDrawerOpen(false)}
+        >
           <ListItemText primary="Contact" />
-        </ListItem>
+        </ListItem> */}
       </List>
       <Divider />
     </div>
@@ -70,7 +100,11 @@ const Header = ({ onProductsClick }) => {
             <MenuIcon />
           </IconButton>
           {/* Drawer */}
-          <Drawer anchor="right" open={drawerOpen} onClose={toggleDrawer(false)}>
+          <Drawer
+            anchor="right"
+            open={drawerOpen}
+            onClose={toggleDrawer(false)}
+          >
             {drawerList()} {/* Render the drawer content */}
           </Drawer>
         </div>
